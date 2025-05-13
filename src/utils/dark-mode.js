0c74b7c154b1f4691f8379100627d49f2e315cea
Donnie <donniesan76@gmail.com>
2025-05-13 19:01:08 +0700

    formatted with prettier

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,25 +1,27 @@
 const darkMode = () => {
-    const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+  const themeToggleBtns = document.querySelectorAll("#theme-toggle");
 
-    // State
-    const theme = localStorage.getItem('theme');
+  // State
+  const theme = localStorage.getItem("theme");
 
-    // On mount
-    theme && document.body.classList.add(theme);
+  // On mount
+  theme && document.body.classList.add(theme);
 
-    // Handlers
-    const handleThemeToggle = () => {
-        document.body.classList.toggle('light-mode');
+  // Handlers
+  const handleThemeToggle = () => {
+    document.body.classList.toggle("light-mode");
 
-        if (document.body.classList.contains('light-mode')) {
-            localStorage.setItem('theme', 'light-mode');
-        } else {
-            localStorage.removeItem('theme');
-            document.body.removeAttribute('class');
-        }
+    if (document.body.classList.contains("light-mode")) {
+      localStorage.setItem("theme", "light-mode");
+    } else {
+      localStorage.removeItem("theme");
+      document.body.removeAttribute("class");
     }
+  };
 
-    themeToggleBtns.forEach(btn => btn.addEventListener('click', handleThemeToggle));
-}
+  themeToggleBtns.forEach((btn) =>
+    btn.addEventListener("click", handleThemeToggle)
+  );
+};
 
 export default darkMode;
