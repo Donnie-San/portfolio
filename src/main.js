603478681bcd21deb56879558ba6a2a5015d0299
Donnie <donniesan76@gmail.com>
2025-05-13 17:23:41 +0700

    Added Contact and Footer Section

--- src/main.js
+++ src/main.js
@@ -4,4 +4,6 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/featured.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
 import '../styles/utils.css';
